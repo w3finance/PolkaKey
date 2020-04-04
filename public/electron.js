7d85d4f68a9c805f3c0399e2a6095c2521097ce8
HiZhaoYun <hizhaoyun@gmail.com>
2020-04-04 22:35:13 +0800

    release v0.1.0

--- public/electron.js
+++ public/electron.js
@@ -75,62 +75,9 @@ log.info('App starting...');
 //     })
 // }
 
-//-------------------------------------------------------------------
-// Open a window that displays the version
-//
-// THIS SECTION IS NOT REQUIRED
-//
-// This isn't required for auto-updates to work, but it's easier
-// for the app to show a window than to have to click "About" to see
-// that updates are working.
-//-------------------------------------------------------------------
-let win;
-
-function sendStatusToWindow(text) {
-    log.info(text);
-    win.webContents.send('message', text);
-}
-
-function createDefaultWindow() {
-    win = new BrowserWindow();
-    win.webContents.openDevTools();
-    win.on('closed', () => {
-        win = null;
-    });
-    win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
-    return win;
-}
-
-autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
-});
-autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available.');
-});
-autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.');
-});
-autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater. ' + err);
-});
-autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
-});
-autoUpdater.on('update-downloaded', (info) => {
-    sendStatusToWindow('Update downloaded');
-});
 app.on('ready', function () {
     //Create the Menu
     // const menu = Menu.buildFromTemplate(template);
     // Menu.setApplicationMenu(menu);
-
-    createDefaultWindow();
-
     autoUpdater.checkForUpdatesAndNotify().then(r => console.info(r));
-});
-app.on('window-all-closed', () => {
-    app.quit();
 });
